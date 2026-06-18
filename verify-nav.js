6c78b35b479bf9edc81a56b40b182f24f05b0b6b
TejasSharma356 <tejassharma3506@gmail.com>
2026-06-18 23:58:16 +0530

    Remove old scripts from root

--- verify-nav.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-const html = fs.readFileSync('public/framer.html', 'utf8');
-console.log('Injected:', html.includes('privitty-nav-injected'));
-console.log('Nav tag found:', html.includes('id="privitty-nav"'));
